90793ae39a294f65a0717e0e641ddb2ed0023edf
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-07-24 02:46:34 -0500

    Update Remesa.js
    Corrección

--- Remesa.js
+++ Remesa.js
@@ -8,10 +8,21 @@
     return el ? el.textContent.trim() : 'N/A';
   };
 
+  const getXPathText = (xpath) => {
+    const result = document.evaluate(
+      xpath,
+      document,
+      null,
+      XPathResult.STRING_TYPE,
+      null
+    );
+    return result.stringValue.trim() || 'N/A';
+  };
+
   const IDcliente = getText('#root > div > div.main-panel.ps.ps--active-y > div.main-content > div:nth-child(1) > div > div > div:nth-child(2) > div:nth-child(2) > p');
   const Tipoderemesa = 'Domicilio';
-  const Povincia = getXPathText('//*[@id="accordion-offers-body-25668313-2"]/div/table/tbody/tr[2]/td/div/div/div[2]/div[3]/p[3]/text()'); // Ejemplo de XPath para IDs dinámicos
-  const NroReparto = getXPathText('//*[@id="accordion-offers-body-25631105-2"]/div/table/tbody/tr[2]/td/div/div/div[3]/div/div/p[1]/text()'); // Ejemplo de XPath para IDs dinámicos
+  const Povincia = getXPathText('//*[@id="accordion-offers-body-25668313-2"]/div/table/tbody/tr[2]/td/div/div/div[2]/div[3]/p[3]/text()');
+  const NroReparto = getXPathText('//*[@id="accordion-offers-body-25631105-2"]/div/table/tbody/tr[2]/td/div/div/div[3]/div/div/p[1]/text()');
   const orderCode = getText('#root > div > div.main-panel.ps.ps--active-y > div.main-content > div:nth-child(1) > div > div > div:nth-child(2) > div:nth-child(1) > p');
   const FoiID = getText('#accordion-offers-body-25631105-2 > div > table > tbody > tr:nth-child(1) > td:nth-child(1)');
   const status = getText('#accordion-offers-body-25631105-2 > div > table > tbody > tr:nth-child(1) > td:nth-child(4)');
@@ -19,7 +30,6 @@
   const solicitud = '';
 
   const resultado = `
-
 ID del cliente: ${IDcliente}
 Tipo de remesa: ${Tipoderemesa}
 Provincia: ${Povincia}
