00a2357ae3604606f06cebc285f69e1bc2388af4
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-07-25 11:34:52 -0500

    Update IdentificadorHTML.js

--- CRM2/Resources/IdentificadorHTML.js
+++ CRM2/Resources/IdentificadorHTML.js
@@ -1,24 +1,14 @@
-(function () {
-  'use strict';
+'use strict';
 
-  // Encuentra el contenido expandido
-  const expandedContent = document.querySelector('.panel-collapse.in');
+const expandedContent = document.querySelector('.panel-collapse.in');
 
-  if (!expandedContent) {
-    console.log("No se encontró ningún bloque expandido.");
-    return;
-  }
-
-  // Encuentra el contenedor principal del panel
+if (!expandedContent) {
+  console.log("No se encontró ningún bloque expandido.");
+  alert("No se encontró ningún bloque expandido.");
+} else {
   const fullPanel = expandedContent.closest('.panel.panel-default');
-
   if (fullPanel) {
-    // Aquí tienes solo el HTML del bloque expandido
     const htmlExpandido = fullPanel.outerHTML;
     console.log(htmlExpandido);
-
-    // Si quieres, puedes retornarlo o usarlo donde lo necesites
-    // Por ejemplo, exportarlo a una función externa:
-    // procesarPanelExpandido(htmlExpandido);
   }
-})();
+}
