8759abbd315271c46411e0b17988ee37970167db
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-07-25 16:32:11 -0500

    Commit from  on 25/7/2025, 16:32:09

--- CRM2/Resources/OrdenExtractor.js
+++ CRM2/Resources/OrdenExtractor.js
@@ -4,7 +4,7 @@
   // 🧪 Verificación: Asegurarse que el bloque HTML esté disponible
   const bloque = window.bloqueElemento;
   if (!bloque) {
-    alert('[OrdenExtractor] ❌ Error: No se encontró el bloque expandido.\n\nNo se encontro la orden con la que estas trabajando.\n\nAsegúrate de seleccionar la orden con la que esta trabajando o verifica o verifica tu ');
+    alert('[OrdenExtractor] ❌ Error: No se encontró el bloque expandido.\n\nNo se encontro la orden con la que estas trabajando.\n\nAsegúrate de depl la orden con la que esta trabajando ó verifica tu conexión de red');
     return;
   }
 
