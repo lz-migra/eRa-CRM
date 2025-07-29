5d34ec6bdf6295234a39a188840ee399fb3a0ab1
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-07-29 16:30:15 -0500

    Commit from  on 29/7/2025, 16:30:14

--- Twilio/cronometroTwilio.js
+++ Twilio/cronometroTwilio.js
@@ -24,7 +24,7 @@
         const hrs = String(Math.floor(segundos / 3600)).padStart(2, '0');
         const mins = String(Math.floor((segundos % 3600) / 60)).padStart(2, '0');
         const secs = String(segundos % 60).padStart(2, '0');
-        return `⏱ ${hrs}:${mins}:${secs}`;
+        return `⏱ ${mins}:${secs}`;
     }
 
     /**
