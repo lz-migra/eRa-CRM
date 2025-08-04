895fbad450251a6fff379c5055c9428d2153d8d4
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-08-04 14:32:28 -0500

    Commit from  on 4/8/2025, 14:32:26

--- Project_croNos/CargadorEntornoTwilio.js
+++ Project_croNos/CargadorEntornoTwilio.js
@@ -9,14 +9,14 @@ IVR
 
 
 CHAT
-- EjecutorCHAT.js
-- CompararMensajeConGuardado.js
+4. EjecutorCHAT.js               https://raw.githubusercontent.com/lz-migra/eRa-CRM/main/Project_croNos/Resourses/CHAT/EjecutorCHAT.js
+- CompararMensajeConGuardado.js 
 - MensajesAgenteStorage.js
 - UltimoMensajeAgente.js
 - detectarNombreAgente.js
 
 GLOBAL
-3. AddRelojes.js       
+3. AddRelojes.js       https://raw.githubusercontent.com/lz-migra/eRa-CRM/main/Project_croNos/Resourses/AddRelojes.js
 2. MonitorTarjetas.js  https://raw.githubusercontent.com/lz-migra/eRa-CRM/main/Project_croNos/Resourses/MonitorTarjetas.js
 1. getTipoDeTarjeta.js https://raw.githubusercontent.com/lz-migra/eRa-CRM/main/Project_croNos/Resourses/getTipoDeTarjeta.js
 
