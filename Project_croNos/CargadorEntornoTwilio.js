0c739ae997c86644c85fd8829edb62a9ce3051db
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-08-04 14:12:28 -0500

    Commit from  on 4/8/2025, 14:12:27

--- Project_croNos/CargadorEntornoTwilio.js
+++ Project_croNos/CargadorEntornoTwilio.js
@@ -1,2 +1,25 @@
 console.log("Cargando funciones globales")
 
+- AddRelojes.js
+
+- CompararMensajeConGuardado.js
+
+- EjecutorCHAT.js
+
+- EjecutorIVR.js
+
+- EjecutorVOISE.js
+
+- MensajesAgenteStorage.js
+
+- MonitorTarjetas.js
+
+- UltimoMensajeAgente.js
+
+- cargarScriptGitHub.js
+
+- detectarNombreAgente.js
+
+- getTipoDeTarjeta.js
+
+- obtenerClaseTarjetaActiva.js
