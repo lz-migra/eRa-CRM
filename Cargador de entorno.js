e165c63817296c263326cf34558d148671524899
LordLoro <47303792+lz-migra@users.noreply.github.com>
2025-08-04 12:21:42 -0500

    Commit from  on 4/8/2025, 12:21:40

---
+++ Cargador de entorno.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Cargador de entorno (Carga remota)
+// @version      1.5
+// @namespace    http://era-crm.local/
+// @description  Agrega herramientas
+// @author       Lorenzo Navarro (Lz-Migra)
+// @match        https://*
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// @license      MIT
+// ==/UserScript==
+
